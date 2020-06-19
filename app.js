4b7c9ccb193612ce5e12af22274eda3f251dab0d
Sharon Lin <hsiang.lin@gmail.com>
2020-06-18 21:28:51 -0400

    Initialize rating app

---
+++ app.js
@@ -0,0 +1,35 @@
+module.exports= {
+
+     smallCar: {
+        engineSize: 4,
+        fuelType: 'diesel',
+    },
+    
+    calculateVehicleRating: function(vehicle) {
+        let rating =0;
+        switch (smallCar.engineSize) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                rating: 10;
+                break;
+            case 5:
+            case 6:
+            case 7:
+            case 8: 
+                rating: 5;
+                break;
+            default: 
+        }
+
+        switch (smallCar.fuelType){
+            case diesel:
+                rating: rating+1; 
+                break;
+            case regular:
+        }
+        return rating;
+        
+    },
+};
---
+++ test/app.spec.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+// reference our app code so we can test it
+// does not yet exist
+const app = require('../app.js')
+
+describe ('Vehicle Rating Calculator', function(){
+    it ('calculate a rating of 10 for a vehicle with 4 cylinders and diesel fuel', function(){
+        const smallCar = app.smallCar;
+        const rating = app.calculateVehicleRating(smallCar);
+        assert.equal(rating, 10);
+    });
+
+});
