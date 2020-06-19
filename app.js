3954d6db6d12151c90d550e36a4dbb21ad11c6f0
Sharon Lin <hsiang.lin@gmail.com>
2020-06-18 23:00:53 -0400

    added smallTruck property and get rating of 6

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
-module.exports= {
+module.exports = {
 
-     smallCar: {
+    smallCar: {
         engineSize: 4,
-        fuelType: 'diesel',
+        fuelType: 'diesel'
     },
-    
-    calculateVehicleRating: function(vehicle) {
-        let rating =0;
-        switch (smallCar.engineSize) {
+
+    smallTruck: {
+        engineSize: 8,
+        fuelType: 'regular'
+    },
+
+
+    calculateVehicleRating: function (vehicle) {
+        let rating = 0;
+
+        switch (vehicle.engineSize) {
             case 1:
             case 2:
             case 3:
             case 4:
-                rating: 8;
+                rating = 8;
                 break;
             case 5:
             case 6:
             case 7:
-            case 8: 
-                rating: 5;
+            case 8:
+                rating = 6;
                 break;
-            default: 
-        }
+        };
 
-        switch (smallCar.fuelType){
-            case diesel:
-                rating: rating+2; 
+        switch (vehicle.fuelType) {
+            case 'diesel':
+                rating = rating + 2;
                 break;
-            case regular:
-        }
-        return rating;
-        
-    },
+            case 'regular':
+        };
+
+        return rating
+
+    }
 };
--- test/app.spec.js
+++ test/app.spec.js
@@ -7,8 +7,13 @@ const app = require('../app.js')
 describe ('Vehicle Rating Calculator', function(){
     it ('calculate a rating of 10 for a vehicle with 4 cylinders and diesel fuel', function(){
         const smallCar = app.smallCar;
-        const rating = app.calculateVehicleRating(app.smallCar);
+        const rating = app.calculateVehicleRating(smallCar);
         assert.equal(rating, 10);
+    });
+    it ('calculate a rating of 6 for a vehicle with 8 cylinders and regular fuel', function(){
+        const smallTruck = app.smallTruck;
+        const rating = app.calculateVehicleRating(smallTruck);
+        assert.equal(rating, 6);
     });
 
 });
